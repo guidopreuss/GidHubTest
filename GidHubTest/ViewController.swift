3e601a7bcdb4cf953f9a03612eff13c23255be08
Guido Preuß <guido@preussonline.de>
2019-01-29 21:24:17 +0100

    Initial Commit

---
+++ GidHubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GidHubTest
+//
+//  Created by Guido Preuß on 29.01.19.
+//  Copyright © 2019 Guido Preuß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GidHubTestTests/GidHubTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  GidHubTestTests.swift
+//  GidHubTestTests
+//
+//  Created by Guido Preuß on 29.01.19.
+//  Copyright © 2019 Guido Preuß. All rights reserved.
+//
+
+import XCTest
+@testable import GidHubTest
+
+class GidHubTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
